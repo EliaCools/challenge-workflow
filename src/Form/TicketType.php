493ce6764043524f79f20f29bd8ed2bb940f7f83
Jenifer Bucheli <jenifer.bucheli@gmail.com>
2021-05-10 16:23:19 +0200

    adding controllers

---
+++ src/Form/TicketType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ticket;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TicketType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('priority')
+            ->add('isEscalated')
+            ->add('dateCreated')
+            ->add('dateClosed')
+            ->add('assignedTo')
+            ->add('createdBy')
+            ->add('status')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ticket::class,
+        ]);
+    }
+}
