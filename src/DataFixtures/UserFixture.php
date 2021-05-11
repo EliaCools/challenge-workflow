e7c93e547a6f097a546afd741f7ee63fc48a8328
Jenifer Bucheli <jenifer.bucheli@gmail.com>
2021-05-11 14:55:53 +0200

    enconded password

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,17 +5,24 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends Fixture
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        //create 2 ROLE_CUSTOMERS
+
 
             $user=new User();
             $user->setEmail('spacebar133%d@example.com');
             $user->setRoles(['ROLE_SECOND_LINE_AGENT']);
-            $user->setPassword('chao01232');
+            $user->setPassword($this->passwordEncoder->encodePassword($user,'new_password'));
             $user->setFirstName('Julio');
             $user->setLastName('Texeira');
             $user->setNumberClosedTickets(2);
