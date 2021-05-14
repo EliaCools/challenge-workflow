d68d13d7d876580846f8b9522d7568d470405859
Julio Cesar Teixeira <juliocesarteixeira1992@gmail.com>
2021-05-14 14:55:56 +0200

    status form incomplete

---
+++ src/Form/TicketReopenType.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Form;
+
+use App\Entity\Status;
+use App\Entity\Ticket;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class TicketReopenType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->setMethod('POST')
+            ->add('Reopen', SubmitType::class, [
+                'label' => 'Reopen Ticket'
+            ]);
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ticket::class,
+        ]);
+    }
+}
